dee54592b312c6cb35eaa431c732980f460d8381
Vladislav <43825581+MurlocMrrgglton@users.noreply.github.com>
2020-03-30 14:23:51 +0300

    init project

---
+++ appVK/appVK/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  appVK
+//
+//  Created by v.sapega on 30.03.2020.
+//  Copyright © 2020 v.sapega. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
